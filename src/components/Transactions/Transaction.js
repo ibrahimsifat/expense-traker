e2c1b64494932bd5dd6b2df791809af3ca59b1d7
ibrahimsifat <ibsifat24@gmail.com>
2022-09-04 00:04:26 +0300

    ad

--- src/components/Transactions/Transaction.js
+++ src/components/Transactions/Transaction.js
@@ -1,25 +1,32 @@
 import { useDispatch } from "react-redux";
 import deleteImage from "../../assets/images/delete.svg";
 import editImage from "../../assets/images/edit.svg";
-import { editActive } from "../../features/transection/transactionSlice";
+import {
+  editActive,
+  removeTransaction,
+} from "../../features/transection/transactionSlice";
+import numberWithCommas from "../../utils/numberWithCommas";
 
 export default function Transaction({ transaction }) {
-  const { name, amount, type } = transaction;
+  const { id, name, amount, type } = transaction;
   const dispatch = useDispatch();
 
   const handleEdit = () => {
     dispatch(editActive(transaction));
+  };
+  const handleDelete = () => {
+    dispatch(removeTransaction(id));
   };
 
   return (
     <li className={`transaction ${type}`}>
       <p>{name}</p>
       <div className="right">
-        <p>৳ {amount}</p>
+        <p>৳ {numberWithCommas(amount)}</p>
         <button onClick={handleEdit} className="link">
           <img alt="Edit" className="icon" src={editImage} />
         </button>
-        <button className="link">
+        <button onClick={handleDelete} className="link">
           <img alt="Delete" className="icon" src={deleteImage} />
         </button>
       </div>
--- src/features/transection/transactionSlice.js
+++ src/features/transection/transactionSlice.js
@@ -116,7 +116,7 @@ const transactionSlice = createSlice({
         state.isError = false;
         state.isLoading = false;
         state.transactions = state.transactions.filter(
-          (t) => t.id !== action.payload
+          (t) => t.id !== action.meta.arg
         );
       })
       .addCase(removeTransaction.rejected, (state, action) => {
